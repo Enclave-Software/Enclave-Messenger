a94dd41f705072f4c2ab0815bea81076a13f48ae
Chinglen2080 <106335829+Chinglen2080@users.noreply.github.com>
2024-12-19 16:35:17 +0530

    Update the idea.py

--- the idea.py
+++ the idea.py
@@ -1,9 +1,27 @@
+#imports
 import requests
+
+#the values
+sent=False
+
+def sent(yes):
+  if yes==True:
+  print("Sent!")
+elif yes==False:
+  print("error")
+
 def encryption(mes):
   enc_mes=mes+"(This message is encrypted)"
   return enc_mes
+
 def send_sms(mes):
-  sem_fin_mes=encryption(mes)
+  fin_mes=encryption(mes)
   return fin_mes
+
 message=str(input("Type your message: "))
-send_sms(message)
+print(send_sms(message))
+if sent==True:
+  print("Sent!")
+elif sent==False:
+  print("error")
+ 
